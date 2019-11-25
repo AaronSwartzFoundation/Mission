b4ea11973b4259c1b5374f10f7390dadbae2b822
AaronSwartzFoundation <58153896+AaronSwartzFoundation@users.noreply.github.com>
2019-11-25 01:50:30 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Science is a Human Right</h1>
+      <h2 />
+      <p>
+        In Article 27 of the Declaration of Human Rights it states in the first
+        part that "Everyone has the right to freely share the benefits of
+        scientific advancement." Which is absolutely true and essential.
+      </p>
+      <p>
+        And it builds on the previous Article 26 where it states that "Everyone
+        has the right to education." And because scientific articles are the
+        main source and only reliable source of human knowledge their reclusive
+        existence behind privately owned company paywalls represents a breach of
+        these human rights. On two accounts.
+      </p>
+      <p>
+        Let this sink in for a moment: The reclusive existence of scientific
+        articles and their findings behind privately owned company paywalls
+        represents a breach of human rights on two accounts.
+      </p>
+      <p>
+        This means that the accusations against Aaron Swartz and the raising of
+        his expected punishment from a year to thirty one years (which is above life
+        sentence in Germany where I come from) for his aim of having two
+        human rights to become reality for everyone with internet access was in and of
+        itself a crime against humanity.
+      </p>
+      <p>
+        To even intend to dole out a sentence that is equivalent to the death
+        penalty which in our country is considered murder is an act so
+        incredulous it makes your hair stand up. But the fact that Aaron upon
+        knowing these insane accusations killed himself amounts to nothing less
+        than man slaughter by neglecting not only his but all humans rights.
+      </p>
+      <p>
+        I hearby call for the immediate efforts to make all human knowledge in
+        the form of all published scientific papers not only illegally but very
+        much legally accessible for everyone by invoking the most basic of all
+        laws that exist: The Declaration of Human Rights that states that all people
+        shall have free access to sciences fruits and education.
+      </p>
+      <p>
+        While there shall be taken measures to move in front of the supreme court  this matter
+        we shall also work on the implementation of a foundation that is dedicated to
+        the realisation of these two quintessential human rights. Quintessential for human self 
+        realisation and thriving.
+      </p>
+      <p>
+        I propose the name of this foundation to be: 
+      </p>
+      <p><b><h2>The Aaron Swartz Foundation</h2></b>
+      </p>
+      <p>
+        It shall strive to implement an open-source platform for scientists around the world
+        to publish their findings, to review other scientists work, to repeat it and to 
+        publicly make available their raw data as well.
+      </p>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
